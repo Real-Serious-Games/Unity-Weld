a41056b9b8fcaa4c28dbaff23da01bc16acaa86e
Rory Dungan <rorydungan@gmail.com>
2017-03-03 11:01:39 +1000

    Bumped version number to 0.2.0

--- UnityWeld_Editor/Properties/AssemblyInfo.cs
+++ UnityWeld_Editor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Real Serious Games")]
-[assembly: AssemblyProduct("UnityUI_Editor")]
+[assembly: AssemblyProduct("UnityWeld_Editor")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
