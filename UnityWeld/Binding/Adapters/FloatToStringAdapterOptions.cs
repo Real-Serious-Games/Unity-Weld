fbcc365d51484fa663e87b370774af7f5431066c
Rory Dungan <rorydungan@gmail.com>
2017-03-09 11:38:28 +1000

    Improved comment on FloatToStringAdapterOptions

--- UnityWeld/Binding/Adapters/FloatToStringAdapterOptions.cs
+++ UnityWeld/Binding/Adapters/FloatToStringAdapterOptions.cs
@@ -2,9 +2,17 @@
 
 namespace UnityWeld.Binding.Adapters
 {
+    /// <summary>
+    /// Options for the float to string adapter.
+    /// </summary>
     [CreateAssetMenu(menuName = "Unity Weld/Adapter options/Float to string adapter")]
     public class FloatToStringAdapterOptions : AdapterOptions
     {
+        /// <summary>
+        /// Options passed in to the Single.ToString() method.
+        /// Defaults to two decimal places.
+        /// See this page for more details: https://msdn.microsoft.com/en-us/library/f71z6k0c(v=vs.110).aspx
+        /// </summary>
         public string Format = "0.00";
     }
 }
