f063fc1cc24f72a54e08fc47bd44a630160edbbe
Rory Dungan <rorydungan@gmail.com>
2017-03-09 11:38:45 +1000

    Added DateTimeToStringAdapter

---
+++ UnityWeld/Binding/Adapters/DateTimeToStringAdapterOptions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityWeld.Binding;
+
+namespace UnityUI.Binding.Adapters
+{
+    /// <summary>
+    /// Options for converting a DateTime to a string.
+    /// </summary>
+    [CreateAssetMenu(menuName = "Unity Weld/Adapter options/DateTime to string adapter")]
+    public class DateTimeToStringAdapterOptions : AdapterOptions
+    {
+        /// <summary>
+        /// Format passed in to the DateTime.ToString method. 
+        /// See this page for details on usage: https://msdn.microsoft.com/en-us/library/zdtaw1bw(v=vs.110).aspx
+        /// </summary>
+        public string Format;
+    }
+}
