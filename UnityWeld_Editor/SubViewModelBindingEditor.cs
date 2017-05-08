a8c88fedbbbd956a4e42db709b7ae87ce250ca32
Rory Dungan <rorydungan@gmail.com>
2017-05-08 16:54:04 +1000

    Wrote an editor for SubViewModelBinding

---
+++ UnityWeld_Editor/SubViewModelBindingEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityWeld.Binding;
+using UnityWeld.Binding.Internal;
+using System.Linq;
+using System.Reflection;
+
+namespace UnityWeld_Editor
+{
+    /// <summary>
+    /// Inspector window for SubViewModelBinding
+    /// </summary>
+    [CustomEditor(typeof(SubViewModelBinding))]
+    public class SubViewModelBindingEditor : BaseBindingEditor
+    {
+        private SubViewModelBinding targetScript;
+
+        private void OnEnable()
+        {
+            targetScript = (SubViewModelBinding)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var bindableProperties = FindBindableProperties();
+
+            ShowViewModelPropertyMenu(
+                new GUIContent("Sub view-model property"),
+                bindableProperties,
+                updatedValue => 
+                {
+                    targetScript.viewModelPropertyName = updatedValue;
+
+                    targetScript.viewModelTypeName = bindableProperties
+                        .Where(prop => MemberInfoToString(prop) == updatedValue)
+                        .Single()
+                        .PropertyType.ToString();
+                },
+                targetScript.viewModelPropertyName,
+                p => true
+            );
+        }
+
+        private PropertyInfo[] FindBindableProperties()
+        {
+            return TypeResolver.FindBindableProperties(targetScript)
+                .Where(property => property
+                    .PropertyType
+                    .GetCustomAttributes(typeof(BindingAttribute), false)
+                    .Any()
+                )
+                .ToArray();
+        }
+    }
+}
