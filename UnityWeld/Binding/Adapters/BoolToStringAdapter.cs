c11092356cfa7757748e2fab5bb38093a1777019
Rory Dungan <rorydungan@gmail.com>
2017-06-21 16:14:40 +1000

    Added adapter for converting from a bool to a string

---
+++ UnityWeld/Binding/Adapters/BoolToStringAdapter.cs
@@ -0,0 +1,16 @@
+﻿namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter for converting from a bool to a string.
+    /// </summary>
+    [Adapter(typeof(bool), typeof(string), typeof(BoolToStringAdapterOptions))]
+    public class BoolToStringAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            var adapterOptions = (BoolToStringAdapterOptions)options;
+
+            return (bool)valueIn ? adapterOptions.TrueValueString : adapterOptions.FalseValueString;
+        }
+    }
+}
