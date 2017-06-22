a08e53f9ba0dccfed807c5e3cfa9f3a4bd4f2d2e
Rory Dungan <rorydungan@gmail.com>
2017-06-22 10:12:26 +1000

    Added adapter to convert from bool to colour

---
+++ UnityWeld/Binding/Adapters/BoolToColorAdapter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter for converting from a bool to a Unity color.
+    /// </summary>
+    [Adapter(typeof(bool), typeof(Color), typeof(BoolToColorAdapterOptions))]
+    public class BoolToColorAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            var adapterOptions = (BoolToColorAdapterOptions)options;
+
+            return (bool)valueIn ? adapterOptions.TrueColor : adapterOptions.FalseColor;
+        }
+    }
+}
