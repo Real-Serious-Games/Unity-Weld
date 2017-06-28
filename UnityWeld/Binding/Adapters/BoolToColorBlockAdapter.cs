ea7f9f2d19a839ad140abf55023086d4b6858a45
Rory Dungan <rorydungan@gmail.com>
2017-06-28 15:06:22 +1000

    Created an adapter for converting from a bool to a ColorBlock

---
+++ UnityWeld/Binding/Adapters/BoolToColorBlockAdapter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+
+namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter for converting from a bool to a Unity color.
+    /// </summary>
+    [Adapter(typeof(bool), typeof(ColorBlock), typeof(BoolToColorBlockAdapterOptions))]
+    public class BoolToColorBlockAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            var adapterOptions = (BoolToColorBlockAdapterOptions)options;
+
+            return (bool)valueIn ? adapterOptions.TrueColors : adapterOptions.FalseColors;
+        }
+    }
+}
