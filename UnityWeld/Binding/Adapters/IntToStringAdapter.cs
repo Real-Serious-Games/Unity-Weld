97c4b4404e24738bc98475213741f5915f1d1a48
Rory Dungan <rorydungan@gmail.com>
2017-07-04 11:29:05 +1000

    Added adapters for converting between ints and strings

---
+++ UnityWeld/Binding/Adapters/IntToStringAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter for converting from an int to a string.
+    /// </summary>
+    [Adapter(typeof(int), typeof(string))]
+    public class IntToStringAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            return ((int)valueIn).ToString();
+        }
+    }
+}
---
+++ UnityWeld/Binding/Adapters/StringToIntAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter for converting from a string to an int.
+    /// </summary>
+    [Adapter(typeof(string), typeof(int))]
+    public class StringToIntAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            return int.Parse((string)valueIn);
+        }
+    }
+}
