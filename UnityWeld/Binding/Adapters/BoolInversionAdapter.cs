7ec9530c22f80969d3d7a3e02f32842aeb8a4311
Rory Dungan <rorydungan@gmail.com>
2017-07-07 10:46:30 +1000

    Added an adapter that inverts the value of a bool passed into it.

---
+++ UnityWeld/Binding/Adapters/BoolInversionAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityWeld.Binding.Adapters
+{
+    /// <summary>
+    /// Adapter that inverts the value of the bound boolean property.
+    /// </summary>
+    [Adapter(typeof(bool), typeof(bool))]
+    public class BoolInversionAdapter : IAdapter
+    {
+        public object Convert(object valueIn, AdapterOptions options)
+        {
+            return !(bool)valueIn;
+        }
+    }
+}
