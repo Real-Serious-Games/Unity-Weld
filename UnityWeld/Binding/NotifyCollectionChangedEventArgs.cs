95ae409da39198333ec512ab6d23e9dc6fb13999
Rory Dungan <rorydungan@gmail.com>
2017-10-11 16:25:40 +1000

    Changed comments in NotifyCollectionChangedEventArgs to standard format

--- UnityWeld/Binding/NotifyCollectionChangedEventArgs.cs
+++ UnityWeld/Binding/NotifyCollectionChangedEventArgs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 
 namespace UnityWeld.Binding
@@ -15,44 +15,49 @@ namespace UnityWeld.Binding
     /// </summary>
     public class NotifyCollectionChangedEventArgs : EventArgs
     {
-        // Summary:
-        //     Gets the action that caused the event.
-        //
-        // Returns:
-        //     A System.Collections.Specialized.NotifyCollectionChangedAction value that
-        //     describes the action that caused the event.
+        /// <summary>
+        /// Gets the action that caused the event.
+        ///
+        /// Returns:
+        ///     A System.Collections.Specialized.NotifyCollectionChangedAction value that
+        ///     describes the action that caused the event.
+        /// </summary>
         public NotifyCollectionChangedAction Action { get; private set; }
 
-        // Summary:
-        //     Gets the list of new items involved in the change.
-        //
-        // Returns:
-        //     The list of new items involved in the change.
+        /// <summary>
+        /// Gets the list of new items involved in the change.
+        ///
+        /// Returns:
+        ///     The list of new items involved in the change.
+        /// </summary>
         public IList NewItems { get; private set; }
 
-        // Summary:
-        //     Gets the index at which the change occurred.
-        //
-        // Returns:
-        //     The zero-based index at which the change occurred.
+        /// <summary>
+        /// Gets the index at which the change occurred.
+        ///
+        /// Returns:
+        ///     The zero-based index at which the change occurred.
+        /// </summary>
         public int NewStartingIndex { get; private set; }
 
-        // Summary:
-        //     Gets the list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
-        //     Remove, or Move action.
-        //
-        // Returns:
-        //     The list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
-        //     Remove, or Move action.
+        /// <summary>
+        /// Gets the list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
+        /// Remove, or Move action.
+        ///
+        /// Returns:
+        ///     The list of items affected by a System.Collections.Specialized.NotifyCollectionChangedAction.Replace,
+        ///     Remove, or Move action.
+        /// </summary>
         public IList OldItems { get; private set; }
 
-        // Summary:
-        //     Gets the index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
-        //     Remove, or Replace action occurred.
-        //
-        // Returns:
-        //     The zero-based index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
-        //     Remove, or Replace action occurred.
+        /// <summary>
+        /// Gets the index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
+        /// Remove, or Replace action occurred.
+        ///
+        /// Returns:
+        ///     The zero-based index at which a System.Collections.Specialized.NotifyCollectionChangedAction.Move,
+        ///     Remove, or Replace action occurred.
+        /// </summary>
         public int OldStartingIndex { get; private set; }
 
         public static NotifyCollectionChangedEventArgs ItemAdded(object item, int index)
